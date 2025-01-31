01e83c19a6b6283e9cced0ca8285c4b515bb7215
Kevin George Buhain <keving.buhain@gmail.com>
2025-01-31 15:47:32 -0500

    add Q5

--- Week04/lab_week4.py
+++ Week04/lab_week4.py
@@ -154,6 +154,14 @@ input("Roll the dice for the monster's health points (Press enter)")
 m_health_points = random.choice(diceOptions)
 print("Player rolled " + str(m_health_points) + " health points for the monster")
 
+# Q5 - item 1
+print("You find a loot bag! look inside to find 2 items")
+input("Roll for the first item (Press Enter)")
+lootRoll = random.choice(range(1, len(loot_options) + 1))
+loot = loot_options.pop(lootRoll - 1)
+belt.append(loot)
+print("Your belt item is: ", belt)
+
 input("Analyze the roll (Press enter)")
 # Compare Player vs Monster's strength
 print("--- You are matched in strength: " + str(combat_strength == m_combat_strength))
