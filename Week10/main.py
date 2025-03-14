7fcbbd3cdfb9b4e254eeadcc55657b9844dc8c71
Kevin George Buhain <keving.buhain@gmail.com>
2025-03-14 15:43:17 -0400

    add week 10 q2

--- Week10/main.py
+++ Week10/main.py
@@ -13,5 +13,15 @@ try:
                     print(row["name"])
             except:
                 print(f"Error Executing Query 1")
+
+            try:
+                del_row = int(input("Enter the row ID threshold for deletion:"))
+                query_2 = "DELETE FROM demo WHERE ID < ?"
+                cursor.execute(query_2, (del_row, ))
+                num_rows = cursor.rowcount
+                print(f"{num_rows} rows affected")
+                db_conn.commit()
+            except:
+                print(f"Error Executing Query 2")
 except sqlite3.Error as e:
     print(f"Databas Connection Error")
